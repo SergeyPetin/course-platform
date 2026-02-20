d7b0737187588a261c3a76bdaba12356ab2396e9
Sergei Petin <petinsergeyn@yandex.ru>
2026-02-20 22:21:57 +0300

    Clean application.properties for Railway DB

--- src/main/java/com/example/courseplatform/CoursePlatformApplication.java
+++ src/main/java/com/example/courseplatform/CoursePlatformApplication.java
@@ -2,11 +2,16 @@ package com.example.courseplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.courseplatform")
+@EnableJpaRepositories(basePackages = "com.example.courseplatform.repository")
 public class CoursePlatformApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoursePlatformApplication.class, args);
     }
 }
+
